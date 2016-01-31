00e58af9d6e6b3c4b0083abecdbb48cb5221381b
Broncobots <broncobots@gmail.com>
2016-01-30 18:53:38 -0600

    Merging Jake's Code

--- src/Commands/WaitForPitch.cpp
+++ src/Commands/WaitForPitch.cpp
@@ -1,15 +1,26 @@
 #include "WaitForPitch.h"
 
-WaitForPitch::WaitForPitch()
+WaitForPitch::WaitForPitch(float setPitch, double speed)
 {
-	// Use Requires() here to declare subsystem dependencies
-	// eg. Requires(chassis);
+	Requires(driveTrain);
+	targetPitch=setPitch;
+	motorSpeed=speed;
 }
 
 // Called just before this Command runs the first time
 void WaitForPitch::Initialize()
 {
-
+//	driveTrain->ResetGyro();
+//	driveTrain->setPID(-0.06, -0.004, 0.0);
+//	driveTrain->SetAutoSpeed(motorSpeed);
+//	driveTrain->SetAutoMode(driveTrain->DRIVE_STRAIGHT);
+	driveTrain->ResetGyro();
+	driveTrain->ResetEncoder();
+	driveTrain->setPID(-0.06, -0.004, 0.0);
+	driveTrain->SetAutoSpeed(motorSpeed);
+	driveTrain->SetAutoMode(driveTrain->DRIVE_STRAIGHT);
+	driveTrain->SetOffset(0.0);
+	driveTrain->Enable();
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -21,18 +32,22 @@ void WaitForPitch::Execute()
 // Make this return true when this Command no longer needs to run execute()
 bool WaitForPitch::IsFinished()
 {
-	return false;
+	return driveTrain->GetRoll() <= targetPitch + 1 && driveTrain->GetRoll() >= targetPitch - 1;   //using roll, not pitch, because reasons
+	std::cout<<driveTrain->GetRoll()<<"         "<<targetPitch<<std::endl;
+	//return driveTrain->GetRoll() <= targetPitch;
 }
 
 // Called once after isFinished returns true
 void WaitForPitch::End()
 {
-
+	driveTrain->Disable();
+	driveTrain->SetAutoSpeed(0);
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void WaitForPitch::Interrupted()
 {
-
+	driveTrain->Disable();
+	driveTrain->SetAutoSpeed(0);
 }
