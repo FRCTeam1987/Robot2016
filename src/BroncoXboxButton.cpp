88a1e56aa6a693770b0d72726d73e8cb329cd4fa
Paul Sites <pts211@gmail.com>
2016-02-18 09:46:32 -0600

    Update BroncoXboxButton.cpp
    Updated to allow for user-defined trigger "triggerPoint" (between 0.0 and 1.0).

--- src/BroncoXboxButton.cpp
+++ src/BroncoXboxButton.cpp
@@ -4,6 +4,7 @@ BroncoXboxButton::BroncoXboxButton(GenericHID *xbox, Button btn) :
 	JoystickButton(xbox, 0) {
 	m_xbox = xbox;
 	m_button = btn;
+	m_triggerPoint = 0.25;
 }
 
 void BroncoXboxButton::Set(const Button newButton)
@@ -11,10 +12,18 @@ void BroncoXboxButton::Set(const Button newButton)
 	m_button = newButton;
 }
 
+void BroncoXboxButton::SetTrigger(const Button newButton, const float triggerPoint)
+{
+	m_button = newButton;
+	
+	//Check to make sure the trigger value is within the acceptable range!
+	if( (triggerPoint >= 0.0) && (triggerPoint <= 1.0) ) {
+	    m_triggerPoint = triggerPoint;
+	}
+}
+
 bool BroncoXboxButton::Get()
 {
-	int modified_id = 0;
-
 	switch(m_button){
 		case A:
 		case B:
@@ -29,10 +38,10 @@ bool BroncoXboxButton::Get()
 			return m_xbox->GetRawButton(m_button);
 			break;
 		case D_U:
-			return (m_xbox->GetPOV() == 0 || m_xbox->GetPOV() == 360); // Need to figure out what the DPAD (POV hat)
+			return (m_xbox->GetPOV() == 0 || m_xbox->GetPOV() == 360); // The D-Pad returns angles in 45 deg. increments, hence the values.
 			break;
 		case D_UR:
-			return (m_xbox->GetPOV() == 45);
+			return (m_xbox->GetPOV() == 45);    // TODO: Try casting the enum to an int and compare to that.
 			break;
 		case D_R:
 			return (m_xbox->GetPOV() == 90);
@@ -53,10 +62,10 @@ bool BroncoXboxButton::Get()
 			return (m_xbox->GetPOV() == 315);
 			break;
 		case LT:
-			return (m_xbox->GetRawAxis(2) > 0.25); //LT is always "axis" number 2
+			return (m_xbox->GetRawAxis(2) > m_triggerPoint); //LT is always "axis" number 2
 			break;
 		case RT:
-			return (m_xbox->GetRawAxis(3)> 0.25); //RT is always "axis" number 3
+			return (m_xbox->GetRawAxis(3)> m_triggerPoint); //RT is always "axis" number 3
 			break;
 		default:
 			return false;
