318c6b7c40329f2de08bb46917448892376d2b29
frc1987 <broncobots@gmail.com>
2016-02-21 20:58:51 -0600

    Update Auto Low Bar
    The command now uses the encoder instead of appearance sensor.

--- src/Commands/DriveTrain/AutoLowBar.cpp
+++ src/Commands/DriveTrain/AutoLowBar.cpp
@@ -1,11 +1,11 @@
 #include "AutoLowBar.h"
 #include "../Collector/SetArmPosition.h"
+#include "DriveStraightForDistance.h"
 #include "DriveStraightUntilRampSensor.h"
-#include "DriveStraightAfterRampSensor.h"
 
 AutoLowBar::AutoLowBar()
 {
 	AddSequential(new SetArmPosition(Collector::kGround));
-	AddSequential(new DriveStraightUntilRampSensor(-0.6));
-	AddSequential(new DriveStraightAfterRampSensor(-0.6));
+	AddSequential(new DriveStraightForDistance(42, -0.6));
+	AddSequential(new DriveStraightUntilRampSensor(0.6));
 }
